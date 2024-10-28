b375feec96c807d39a5707ce15d19805dbb5a4b5
DuckDuckGo-prog <weql4143@duck.com>
2024-10-28 12:30:29 +0800

    Update text_render.py
    Canvas Border Fix

--- manga_translator/rendering/text_render.py
+++ manga_translator/rendering/text_render.py
@@ -737,10 +737,7 @@ def put_text_horizontal(font_size: int, text: str, width: int, height: int, alig
     line_box = add_color(canvas_text, fg, canvas_border, bg)
 
     # rect
-    if bg is None :
-        x, y, w, h = cv2.boundingRect(canvas_text)
-    else :
-        x, y, w, h = cv2.boundingRect(canvas_border)
+    x, y, width, height = cv2.boundingRect(canvas_border)
     return line_box[y:y+height, x:x+width]
 
 # def put_text(img: np.ndarray, text: str, line_count: int, x: int, y: int, w: int, h: int, fg: Tuple[int, int, int], bg: Optional[Tuple[int, int, int]]):
