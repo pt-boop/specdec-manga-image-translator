9d59f11a3e96307b59104d06ff68f73458ec4653
DuckDuckGo-prog <weql4143@duck.com>
2024-11-25 06:14:34 +0800

    Update groq.py
    change from llama3-70b-8192 to llama-3.2-90b-text-preview

--- manga_translator/translators/groq.py
+++ manga_translator/translators/groq.py
@@ -57,7 +57,7 @@ class GroqTranslator(CommonTranslator):
         self.token_count = 0
         self.token_count_last = 0
         self.config = None
-        self.model = os.environ.get('GROQ_MODEL', 'llama3-70b-8192')
+        self.model = os.environ.get('GROQ_MODEL', 'llama-3.2-90b-text-preview')
         self.messages = [
             {'role': 'user', 'content': self.chat_sample[0]},
             {'role': 'assistant', 'content': self.chat_sample[1]}]
